712a606265ef141d940d8f9612d9ad1081b91c10
Schtrafzer <Schtrafzer@gmail.com>
2020-05-08 13:53:20 +0700

    Добавьте файлы проекта.

---
+++ MiAPO_Lab3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiAPO_Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длинну ребра куба в сантиметрах:");
+            double A = Convert.ToDouble(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("Объем куба равен " + Math.Pow(A, 3) + " кубических сантиметров.");
+            Console.ReadLine();
+        }
+    }
+}
